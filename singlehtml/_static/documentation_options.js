d498a2e1bdf9d53ccad90116664c697de6774e3d
wmamills <wm.a.mills@gmail.com>
2021-05-31 15:28:55 +0000

    Deploying to gh-pages from @ wmamills/ebbr@a2bc03db732e4f73b367703256911f9e86557688 🚀

--- singlehtml/_static/documentation_options.js
+++ singlehtml/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.0.0-29-ga467838',
+    VERSION: 'v2.0.0-34-ga2bc03d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
